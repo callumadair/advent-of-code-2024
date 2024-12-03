9c211000b16513661edf11c52888a2290106103c
Callum Adair <callum.adair@outlook.com>
2024-12-03 20:21:15 +0000

    made potentially sketch solution for day 2 part 2

--- two/src/main.rs
+++ two/src/main.rs
@@ -3,6 +3,7 @@ use std::{
     io::{
         BufRead,
         BufReader,
+        Lines,
     },
     path::Path,
     str::FromStr,
@@ -16,27 +17,27 @@ const INPUT_LINE_DELIMITER: &str = " ";
 fn main()
 {
     let safety_value =
-        read_safety_input(Path::new("./src/input.txt")).expect("Failed calculating safety total.");
+        calculate_safety(Path::new("./src/input.txt")).expect("Failed calculating safety total.");
     println!("The safety value for this input is: {safety_value}");
+    let safety_value = calculate_safety_with_dampener(Path::new("./src/input.txt"))
+        .expect("Failed calculating safety total with safety dampener.");
+    println!("The safety value for this input with the safety dampener is: {safety_value}");
 }
 
-fn read_safety_input(path: &Path) -> Result<usize>
+fn get_lines(path: &Path) -> Result<Lines<BufReader<File>>>
+{
+    let input_file = File::open(path)?;
+    let buf_reader = BufReader::new(input_file);
+    Ok(buf_reader.lines())
+}
+
+fn calculate_safety(path: &Path) -> Result<usize>
 {
     let mut safety_count = 0_usize;
 
-    let input_file = File::open(path)?;
-    let buf_reader = BufReader::new(input_file);
-
-    'line_reader: for line in buf_reader.lines()
+    'line_reader: for line in get_lines(path)?
     {
-        // Parse the line into a vec of usize vals.
-        let unwrapped_line = line?;
-        let line_vals = unwrapped_line
-            .split(INPUT_LINE_DELIMITER)
-            .collect::<Vec<&str>>()
-            .iter()
-            .map(|val| usize::from_str(val).unwrap())
-            .collect::<Vec<usize>>();
+        let line_vals = collect_line_vals(line?)?;
 
         for i in 1..line_vals.len() - 1
         {
@@ -54,6 +55,95 @@ fn read_safety_input(path: &Path) -> Result<usize>
             }
         }
         safety_count += 1;
+    }
+
+    Ok(safety_count)
+}
+
+fn collect_line_vals(line: String) -> Result<Vec<usize>>
+{
+    // Parse the line into a vec of usize vals.
+    let line_vals = line
+        .split(INPUT_LINE_DELIMITER)
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|val| usize::from_str(val).unwrap())
+        .collect::<Vec<usize>>();
+    Ok(line_vals)
+}
+
+enum ValueDirection
+{
+    None,
+    Ascending,
+    Descending,
+}
+
+impl ValueDirection
+{
+    fn is_assigned(&self) -> bool
+    {
+        match *self
+        {
+            ValueDirection::None => false,
+            ValueDirection::Ascending => true,
+            ValueDirection::Descending => true,
+        }
+    }
+}
+
+fn calculate_safety_with_dampener(path: &Path) -> Result<usize>
+{
+    let mut safety_count = 0;
+    'line_reader: for line in get_lines(path)?
+    {
+        let line_vals = collect_line_vals(line?)?;
+        let mut direction = ValueDirection::None;
+        let mut unsafety_count = 0;
+        for i in 1..line_vals.len()
+        {
+            let last = line_vals[i - 1];
+            let cur = line_vals[i];
+            match direction
+            {
+                ValueDirection::None =>
+                {
+                    if cur > last
+                    {
+                        direction = ValueDirection::Ascending;
+                    }
+
+                    if cur < last
+                    {
+                        direction = ValueDirection::Descending
+                    }
+                }
+                ValueDirection::Ascending =>
+                {
+                    if cur < last
+                    {
+                        unsafety_count += 1;
+                    }
+                }
+                ValueDirection::Descending =>
+                {
+                    if cur > last
+                    {
+                        unsafety_count += 1;
+                    }
+                }
+            }
+
+            let diff = cur.abs_diff(last);
+            if !(1..=3).contains(&diff)
+            {
+                unsafety_count += 1;
+            }
+        }
+        if unsafety_count <= 1
+        {
+            safety_count += 1;
+        }
     }
 
     Ok(safety_count)

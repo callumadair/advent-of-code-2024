607b6cebefa9f57fd7e4dde0dea8cf466716fe64
Callum Adair <callum.adair@outlook.com>
2024-12-06 21:15:33 +0000

    Completed day 3

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,22 +1,27 @@
 use std::{
     fs::File,
-    io::{
-        BufReader,
-        Read,
-    },
+    io::Read,
     path::Path,
     str::FromStr,
 };
 
 use color_eyre::Result;
-use regex::Regex;
+use regex::{
+    Match,
+    Regex,
+};
 
 fn main()
 {
     let file_contents =
         read_file(Path::new("./src/input.txt")).expect("Failed reading input file.");
-    let total = calculate_product_total(file_contents).expect("Failed calculating total.");
-    println!("Total is: {total}");
+    let part_two_total =
+        calculate_product_total(&file_contents).expect("Failed calculating total.");
+    println!("Part One Total is: {part_two_total}");
+
+    let part_two_total = calculate_product_total_with_conditionals(&file_contents)
+        .expect("Failed calculating total with conditionals");
+    println!("Part Two Total is: {part_two_total}");
 }
 fn read_file(path: &Path) -> Result<String>
 {
@@ -27,13 +32,17 @@ fn read_file(path: &Path) -> Result<String>
     Ok(output_string)
 }
 
-fn calculate_product_total(input: String) -> Result<usize>
+fn calculate_product_total(input: &str) -> Result<usize>
 {
     let re = Regex::new(r"mul\(\d+,\d+\)")?;
-    let matches = re.find_iter(&input).collect::<Vec<_>>();
-    dbg!(&matches);
+    let matches = re.find_iter(input).collect::<Vec<Match>>();
+    parse_inner_regex(matches)
+}
+
+fn parse_inner_regex(matched_outer_regex: Vec<Match>) -> Result<usize>
+{
     let inner_re = Regex::new(r"\d+")?;
-    let total = matches
+    let total = matched_outer_regex
         .iter()
         .map(|val| {
             let inner_matches = inner_re
@@ -47,6 +56,36 @@ fn calculate_product_total(input: String) -> Result<usize>
             nums.iter().product::<usize>()
         })
         .sum();
+    Ok(total)
+}
+
+fn calculate_product_total_with_conditionals(input: &str) -> Result<usize>
+{
+    let conditional_regex =
+        Regex::new(r"mul\((?<left>\d+),(?<right>\d+)\)|(?<yes>do\(\))|(?<no>don't\(\))")?;
+    let total = {
+        let mut activated = true;
+        let mut total = 0;
+        for capture in conditional_regex.captures_iter(input)
+        {
+            if capture.name("yes").is_some()
+            {
+                activated = true;
+                continue;
+            }
+            if capture.name("no").is_some()
+            {
+                activated = false;
+                continue;
+            }
+
+            if activated
+            {
+                total += &capture["left"].parse::<usize>()? * &capture["right"].parse::<usize>()?;
+            }
+        }
+        total
+    };
 
     Ok(total)
 }
